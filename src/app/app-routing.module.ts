441c18dcb72d72eb35e4e239caeb201bc4a968a9
Daniel Inche <damn1self@gmail.com>
2025-01-14 18:05:02 -0500

    LazyLoading carga por bloques segun se requiera

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path :'home',//LOCALHOST:4200/home/dashboard
+  loadChildren:() => import('./modules/home/home.module').then(m=>m.HomeModule)
+}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
