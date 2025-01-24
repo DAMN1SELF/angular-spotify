67b6dee8ce095c04c795804984b5df6fa07590bf
Daniel Inche <damn1self@gmail.com>
2025-01-23 22:27:05 -0500

    Raw: {{frmLogin.value|json}}

--- src/app/modules/auth/pages/auth-page/auth-page.component.ts
+++ src/app/modules/auth/pages/auth-page/auth-page.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-auth-page',
   templateUrl: './auth-page.component.html',
   styleUrls: ['./auth-page.component.css']
 })
-export class AuthPageComponent {
+export class AuthPageComponent implements OnInit{
+
+  frmLogin:FormGroup = new FormGroup({});
+  constructor(){
+  }
+  ngOnInit(): void {
+    this.frmLogin=new FormGroup({
+      email: new FormControl(''),
+      password:new FormControl('')
+    })
+  }
 
 }
