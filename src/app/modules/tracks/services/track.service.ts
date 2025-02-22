3859c8c4fbeee05bf89d82f99639277b93b52c0b
Daniel Inche <damn1self@gmail.com>
2025-02-22 09:31:59 -0500

    quitar sub local

--- src/app/modules/tracks/services/track.service.ts
+++ src/app/modules/tracks/services/track.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { TrackModel } from '@core/models/tracks.models';
 import { Observable, of } from 'rxjs';
-import * as dataRaw from '../../../data/tracks.json';
 
 @Injectable({
   providedIn: 'root'
@@ -13,22 +12,6 @@ export class TrackService {
   dataTracksSaved$:Observable<TrackModel[]>=of([])
 
   constructor() {
-    const { data }: any = (dataRaw as any).default;
-    this.dataTracksTrending$ = of(data)
-
-    this.dataTracksRandom$ = new Observable((observer) => {
-      const trackExample:TrackModel = {
-        _id: '1',
-        name  : 'Track 1',
-        album : 'Album 1',
-        url : 'https://www.youtube.com/watch?v=1',
-        cover: 'https://i.ytimg.com/vi/1/mqdefault.jpg',
-      }
-      setTimeout(() =>
-        {
-          observer.next([trackExample])
-        },5000)
-    })
   }
 
 }
