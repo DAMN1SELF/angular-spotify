5e9c6de84fe6357b345c1c2acd24586be0428cfe
Daniel Inche <damn1self@gmail.com>
2025-03-06 00:44:21 -0500

    cookie

--- src/app/modules/auth/services/auth.service.ts
+++ src/app/modules/auth/services/auth.service.ts
@@ -1,7 +1,9 @@
+import { tap } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { CookieService } from 'ngx-cookie-service';
 
 
 @Injectable({
@@ -11,14 +13,18 @@ export class AuthService {
 
   private readonly URL=environment.api;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,private cookie:CookieService) { }
 
   enviarLogin(email:string,password:string):Observable<any>{
     const body={
       email,password
     }
-    return this.http.post(`${this.URL}/auth/login`,body).pipe(
-
+    return this.http.post(`${this.URL}/auth/login`,body)
+    .pipe(
+      tap((responseOk:any) => {
+        const {tokenSession,data}=responseOk
+        this.cookie.set('token_service',tokenSession,4,'/')
+      })
     )
   }
 }
